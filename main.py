19ac0731cfcf5a20971f462b74bafdfd09714172
vjindal50 <vjindal@stevens.edu>
2024-12-31 10:38:16 -0600

    Stock comparison agent

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
 from dotenv import load_dotenv
 
 load_dotenv()
 
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile")
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
+    show_tool_calls=True,
+    markdown=True,
+    instructions=["use tables to display data, and dont truncate the data"]
 )
 
-agent.print_response("write a 2 line poem on money and poverty")
+agent.print_response("Summarize and compare analyst recommendations and fundamentals for GRMN and TSLA")
