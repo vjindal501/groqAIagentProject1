3dfd7f766b518ed404d3be16e132235f5367ca07
vjindal50 <vjindal@stevens.edu>
2024-12-31 01:39:59 -0600

    Inital commit after test run

---
+++ main.py
@@ -0,0 +1,11 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+agent = Agent(
+    model=Groq(id="llama-3.3-70b-versatile")
+)
+
+agent.print_response("write a 2 line poem on money and poverty")
